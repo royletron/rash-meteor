285e50f85cf8d9a5573e769cac8d2d591d399d99
Darren <royletron@gmail.com>
2013-06-04 19:07:18 +0000

    Added maps

--- client/router.js
+++ client/router.js
@@ -8,12 +8,23 @@ function logout() {
 	}
 }
 
+function load() {
+  if (Meteor.loggingIn()) {
+    // dynamically set the template
+    this.template("loading");
+    // stop downstream callbacks from running
+    this.done();
+  }
+}
+
 Meteor.pages({
 
     // Page values can be an object of options, a function or a template name string
 
     '/': { to: 'home', as: 'root', nav: 'home' },
     '/places': { to: 'places', as: 'places', nav: 'places' },
+    '/myplaces': { to: 'myplaces', as: 'myplaces', nav: 'myplaces', layout: 'inverselayout' },
+    '/places/new': { to: 'addplace', as: 'addplace', nav: 'addplace' },
     '/login': { to: 'login', as: 'login', nav: 'login' },
     '/logout': { to: 'logout', as: 'logout', nav: 'logout', before: [logout] },
     '/signup': { to: 'signup', as: 'signup', nav: 'signup' }
@@ -21,6 +32,7 @@ Meteor.pages({
 
     // optional options to pass to the PageRouter
     defaults: {
-      layout: 'layout'
+      layout: 'layout',
+      before: [load]
     }
   });
---
+++ client/views/places.js
@@ -0,0 +1,22 @@
+Template.myplaces.rendered = function() {
+  var mapOptions = {
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+        position: google.maps.ControlPosition.BOTTOM_CENTER
+    },
+    panControl: true,
+    panControlOptions: {
+        position: google.maps.ControlPosition.LEFT_CENTER
+    },
+    zoomControl: true,
+    zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.LARGE,
+        position: google.maps.ControlPosition.LEFT_CENTER
+    },
+    scaleControl: false
+  };
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+}
